b660191cd1d4a4b62d358fa2be0a5e6bbc0659fe
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-04-05 17:57:53 -0400

    Sync and exit if failed command

--- src/builderl.erl
+++ src/builderl.erl
@@ -47,7 +47,7 @@ execute_stages([], _Dir) ->
 
 execute_steps([Step|Steps], Dir) ->
     io:format("Step: ~p~n", [Step]),
-    exec:run(Step, [{stdout, print}, {cd, Dir}]),
+    {ok, _} = exec:run(Step, [{stdout, print}, {cd, Dir}, sync]),
     execute_steps(Steps, Dir),
     ok;
 execute_steps([], _Dir) ->
