77f624a457e59556b8c20738731d0222a517bfc3
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-06-27 18:04:39 -0400

    Update cert only if expiring soon
    https://letsencrypt.org/docs/integration-guide/ recommends only renewing when within 30 days of expiry. Read the cert and only renew if we are < 31 days.

--- src/builderl_app.erl
+++ src/builderl_app.erl
@@ -2,10 +2,16 @@
 -behaviour(application).
 -compile({parse_transform, lager_transform}).
 
+-include_lib("public_key/include/public_key.hrl").
+
 -export([start/2]).
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    SU = should_update_cert("crt.crt"),
+    lager:info("Key info ~p", [SU]),
+    SU2 = should_update_cert("crt2.crt"),
+    lager:info("Key info ~p", [SU2]),
     case application:get_env(builderl, ssl, false) of
         letsencrypt -> setup_cowboy_with_letsencrypt();
         _ -> setup_cowboy_http()
@@ -62,12 +68,50 @@ https_redirect_routes() ->
 start_letsencrypt() ->
     {ok, Domain} = application:get_env(builderl, domain),
     {ok, CertDir} = application:get_env(builderl, certificates_directory),
-    BDomain = binary:list_to_bin(Domain),
-    {ok, _Pid} = letsencrypt:start([{mode,slave}, {cert_path, CertDir}]),
-    MC = letsencrypt:make_cert(BDomain, #{async => false}),
-    {ok, CertMap} = MC,
-    lager:info("Lets Encrypt ~p", [MC]),
-    CertProps = [{certfile, binary:bin_to_list(maps:get(cert, CertMap))},
-                 {keyfile, binary:bin_to_list(maps:get(key, CertMap))}],
+    CertFile = CertDir ++ "/" ++ Domain ++ ".crt",
+
+    CertProps = case should_update_cert(CertFile) of
+        true -> BDomain = binary:list_to_bin(Domain),
+                {ok, _Pid} = letsencrypt:start([{mode,slave}, {cert_path, CertDir}]),
+                MC = letsencrypt:make_cert(BDomain, #{async => false}),
+                {ok, CertMap} = MC,
+                lager:info("Lets Encrypt ~p", [MC]),
+                [{certfile, binary:bin_to_list(maps:get(cert, CertMap))},
+                 {keyfile, binary:bin_to_list(maps:get(key, CertMap))}];
+        false -> [{certfile, binary:bin_to_list(CertDir ++ "/" ++ Domain ++ ".crt")},
+                 {keyfile, binary:bin_to_list(CertDir ++ "/" ++ Domain ++ ".key")}]
+    end,
 
     CertProps.
+
+should_update_cert(Path) ->
+    File = file:read_file(Path),
+    should_update_cert_from_file(File).
+
+should_update_cert_from_file({error,enoent}) ->
+    true;
+should_update_cert_from_file({ok, File}) ->
+    [Cert1| _] = public_key:pem_decode(File),
+    CertData = public_key:pem_entry_decode(Cert1),
+    TBSC = CertData#'Certificate'.tbsCertificate,
+    Validity = TBSC#'TBSCertificate'.validity,
+    {utcTime, ValidUntil} = Validity#'Validity'.notAfter,
+
+    SY = string:sub_string(ValidUntil, 1, 2),
+    SM = string:sub_string(ValidUntil, 3, 4),
+    SD = string:sub_string(ValidUntil, 5, 6),
+
+    {IY, _} = string:to_integer(SY),
+    {IM, _} = string:to_integer(SM),
+    {ID, _} = string:to_integer(SD),
+
+    ValidUntilDate = {IY + 2000, IM, ID},
+    ValidUntilDay = calendar:date_to_gregorian_days(ValidUntilDate),
+    {Today, _} = calendar:universal_time(),
+    DaysUntilToday = calendar:date_to_gregorian_days(Today),
+    DaysRemaining =  ValidUntilDay - DaysUntilToday,
+
+    if DaysRemaining < 31 -> true;
+       true               -> false
+    end.
+
