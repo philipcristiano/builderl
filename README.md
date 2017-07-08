f92f6672307abb51484036fd53c21b29402f56d1
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-07-08 14:50:50 -0400

    webhook: Validate Github hmac
    Validate the hash that Github sends as a header if the config for a project has a GitHub secret in the local config.

--- README.md
+++ README.md
@@ -58,8 +58,22 @@ The HTTP port used by the web server can be changed with `port` (default 8080 in
 
 `projects` - A list of Github Org/Repo strings for whitelisted projects to build and their options
 
+Options:
+
+* github_webhook_secret - [The secret supplied to Github to verify webhooks](https://developer.github.com/webhooks/securing/)
+
+Minimal example:
 ```
 [{projects, [{"philipcristiano/builderl", []}]}].
+```
+
+Full example:
+
+```
+[{projects, [{"philipcristiano/builderl", [
+                {github_webhook_secret, "$SECRET"}
+            ]}]
+}].
 ```
 
 `builds_directory` - Location to checkout/build repositories. Defaults to `/tmp` locally or `/var/lib/builderl/builds` when packaged.
@@ -93,7 +107,9 @@ Example Configuration
   {ssl, letsencrypt},
   {domain, "builderl.example.com"},
   {http_port, 80},
-  {projects, ["philipcristiano/builderl"]},
+  {projects, [{"philipcristiano/builderl", [
+                {github_webhook_secret, "$SECRET"}
+  ]}]},
   {global_env, [{"PATH","/usr/gnu/bin:/opt/omni/bin/:/opt/gcc-5.1.0/bin:/usr/bin:/usr/sbin:/sbin"}]
   }]
  },
--- src/builderl_github_webhook.erl
+++ src/builderl_github_webhook.erl
@@ -5,6 +5,12 @@
 -export([init/2,
          state_init/0]).
 
+-record(push_event, {full_name,
+                     url,
+                     ref,
+                     committish}).
+
+
 state_init() ->
     Projects = builderl:get_projects(),
     lager:info("Whitelisted projects ~p", [Projects]),
@@ -13,20 +19,48 @@ state_init() ->
 init(Req0=#{method := <<"POST">>}, State) ->
     {ok, Body, Req1} = cowboy_req:read_body(Req0),
     Projects = proplists:get_value(projects, State),
-    Data = jsx:decode(Body, [return_maps]),
+    lager:debug("Body ~p", [Req0]),
+    {ok, PE} = post_body_to_record(Body),
 
-    % Validate project is in whitelist
-    Repo = maps:get(<<"repository">>, Data, #{}),
-    FullName = maps:get(<<"full_name">>, Repo),
-    IsMember = lists:member(FullName, Projects),
+    GHSignature = cowboy_req:header(<<"x-hub-signature">>, Req1),
+    Secret = builderl:get_project_config_value(PE#push_event.full_name, github_webhook_secret),
 
-    Resp = handle_webhook(IsMember, FullName, Data, Repo, Req1),
+    Resp = case valid_signature(GHSignature, Secret, Body) of
+        false -> lager:warning("Invalid signature for project ~p with sig ~p",
+                               [PE#push_event.full_name, GHSignature]),
+                 cowboy_req:reply(400,
+                                  #{<<"content-type">> => <<"text/plain">>},
+                                    <<"Invalid signature">>, Req1);
+        true  -> IsMember = lists:member(PE#push_event.full_name, Projects),
+                 handle_whitelist_member(IsMember, PE, Req1)
+    end,
     {ok, Resp, State}.
 
-handle_webhook(true, FullName, Data, Repo, Req) ->
+valid_signature(_Signature, undefined, _Body) ->
+    lager:debug("No signature for us :/"),
+    true;
+valid_signature(Signature, Secret, Body) when is_list(Secret)->
+    <<Mac:160/integer>> = crypto:hmac(sha, binary:list_to_bin(Secret), Body),
+    OurHash = binary:list_to_bin(lists:flatten(io_lib:format("~40.16.0b", [Mac]))),
+    OurSig = << <<"sha1=">>/binary, OurHash/binary>>,
+    lager:debug("Our signature ~p, their signature ~p", [OurSig, Signature]),
+    OurSig == Signature.
+
+post_body_to_record(Body) ->
+    Data = jsx:decode(Body, [return_maps]),
+    Repo = maps:get(<<"repository">>, Data, #{}),
     Url = maps:get(<<"url">>, Repo),
     Ref = maps:get(<<"ref">>, Data),
     CommitIsh = binary:bin_to_list(maps:get(<<"after">>, Data)),
+    FullName = maps:get(<<"full_name">>, Repo),
+
+    {ok, #push_event{full_name=FullName,
+                     url=Url,
+                     ref=Ref,
+                     committish=CommitIsh}}.
+
+
+handle_whitelist_member(true, #push_event{full_name=FullName, url=Url, ref=Ref, committish=CommitIsh}, Req) ->
 
     Req2 = cowboy_req:reply(200,
         #{<<"content-type">> => <<"text/plain">>},
@@ -38,7 +72,7 @@ handle_webhook(true, FullName, Data, Repo, Req) ->
     trigger_build(NameL, Url, CommitIsh, LRef),
     Req2;
 
-handle_webhook(false, FullName, _Data, _Repo, Req) ->
+handle_whitelist_member(false, #push_event{full_name=FullName}, Req) ->
     Req2 = cowboy_req:reply(400,
         #{<<"content-type">> => <<"text/plain">>},
         << FullName/binary, <<" is not in the builderl whitelist">>/binary >>,
