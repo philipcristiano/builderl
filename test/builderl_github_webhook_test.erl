8f8d55297d058291970cefb41ffde6f32880f03f
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-07-12 08:44:55 -0400

    test: No sig configured and not whitelisted

--- test/builderl_github_webhook_test.erl
+++ test/builderl_github_webhook_test.erl
@@ -1,8 +1,9 @@
 -module(builderl_github_webhook_test).
+-compile({parse_transform, lager_transform}).
+-compile(export_all).
 
 -include_lib("eunit/include/eunit.hrl").
 
--compile(export_all).
 
 -define(MUT, builderl_github_webhook).
 
@@ -16,7 +17,17 @@ non_whitelist_test_() ->
     {setup,
      fun setup_handler/0,
      fun teardown_handler/1,
-     fun invalid_signature/1}.
+     fun (Config) ->
+         [invalid_signature(Config)]
+     end}.
+
+no_configured_signature_test_() ->
+    {setup,
+     fun setup_no_sig_handler/0,
+     fun teardown_handler/1,
+     fun (Config) ->
+          [no_signature_configured_not_whitelisted(Config)]
+     end}.
 
 setup_handler() ->
     application:ensure_all_started(lager),
@@ -26,6 +37,17 @@ setup_handler() ->
     ok = meck:new(cowboy_req, []),
     ok = meck:expect(builderl, get_projects, fun() -> [project_1] end),
     ok = meck:expect(builderl, get_project_config_value, fun(_, _) -> "VALUE" end),
+
+    [].
+
+setup_no_sig_handler() ->
+    application:ensure_all_started(lager),
+    ok = lager_common_test_backend:bounce(debug),
+
+    ok = meck:new(builderl, []),
+    ok = meck:new(cowboy_req, []),
+    ok = meck:expect(builderl, get_projects, fun() -> [project_1] end),
+    ok = meck:expect(builderl, get_project_config_value, fun(_, _) -> undefined end),
 
     [].
 
@@ -60,8 +82,36 @@ invalid_signature(_Config) ->
     [?_assertEqual(Ref, RRef),
      ?_assertEqual(<<"Invalid signature">>, Body)].
 
+no_signature_configured_not_whitelisted(_Config) ->
+    State0 = ?MUT:state_init(),
+    PostReq = #{method => <<"POST">>} ,
+
+    Data = #{<<"repository">> =>
+                #{<<"url">> => <<"repo_url">>,
+                  <<"full_name">> => <<"test_full_name">>
+                },
+             <<"ref">> => <<"test_ref">>,
+             <<"after">> => <<"after_sha">>
+    },
+
+    ok = meck:expect(cowboy_req, read_body, fun(Req) -> make_json_response(Data, Req) end),
+    ok = meck:expect(cowboy_req, header, fun(_, _) -> undefined end),
+
+    Ref = make_ref(),
+    ok = meck:expect(cowboy_req, reply, fun(400, _, Body, _Req) -> {Ref, Body} end),
+    {ok, {RRef, Body}, _} = ?MUT:init(PostReq, State0),
+    DoesMatch = re_match(Body, ".*not in the builderl whitelist.*"),
+    [?_assertEqual(Ref, RRef),
+     ?_assert(DoesMatch)].
 
 
 make_json_response(Data, Req) ->
     Json = jsx:encode(Data),
     {ok, Json, Req}.
+
+re_match(Subject, RE) ->
+    case re:run(Subject, RE) of
+        nomatch -> ok = lager:info("~p does not match ~p", [RE, Subject]),
+                   false;
+        {match, _} -> true
+    end.
