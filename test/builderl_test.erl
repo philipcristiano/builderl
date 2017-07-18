dc62333a6278b5e7256fa1a2a2c09025d9474104
Philip Cristiano <philipcristiano@philipcristiano.com>
2017-07-18 17:04:29 -0400

    test: config validation happy path

--- test/builderl_test.erl
+++ test/builderl_test.erl
@@ -8,13 +8,19 @@
 -define(MUT, builderl).
 
 validate_builderl_build_config_test_() ->
-    [non_string_environment_config()].
+    [non_string_environment_config(),
+     valid_config()].
 
 non_string_environment_config() ->
     {error, ErrMsg} = ?MUT:validate_builderl_config([{"environment", [{"foo", bar}]}]),
     DoesMatch = re_match(ErrMsg, "Environment variables must be strings"),
 
     [?_assert(DoesMatch)].
+
+valid_config() ->
+    Config = [{"environment", [{"foo", "bar"}]}],
+    Resp = ?MUT:validate_builderl_config(Config),
+    [?_assertEqual(ok, Resp)].
 
 re_match(Subject, RE) ->
     case re:run(Subject, RE) of
