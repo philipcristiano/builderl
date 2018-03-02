b063d303a965494f0af1ad2e32de1d7c13f0ceb2
Philip Cristiano <philipcristiano@philipcristiano.com>
2018-03-02 18:23:20 -0500

    sessions: Store in ETS

---
+++ src/builderl_ets_session_store.erl
@@ -0,0 +1,154 @@
+%%%-------------------------------------------------------------------
+%%% @author philipcristiano
+%%% @copyright 2018 philipcristiano
+%%% @doc
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+
+-module(builderl_ets_session_store).
+
+-behaviour(gen_server).
+
+%% API functions
+-export([start_link/0,
+         set_value/3,
+         get_value/2,
+         get_value/3]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-record(state, {}).
+
+-define(TABLE, builderl_session_store).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+set_value(Key, Value, SessionID) ->
+    gen_server:call(?MODULE, {set_value, Key, Value, SessionID}).
+
+get_value(Key, SessionID) ->
+    get_value(Key, SessionID, undefined).
+
+get_value(Key, SessionID, Default) ->
+    Value = ets:match(builderl_session_store, {{SessionID, Key}, '$1'}),
+    matched_value(Value, Default).
+
+matched_value([], Default) ->
+    Default;
+matched_value([[Val]], _Default) ->
+    Val.
+
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    ?TABLE = ets:new(?TABLE, [set, named_table]),
+    {ok, #state{}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call({set_value, Key, Value, SessionID}, _From, State) ->
+    ets:insert(?TABLE, {{SessionID, Key}, Value}),
+    {reply, ok, State};
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
--- src/builderl_sessions.erl
+++ src/builderl_sessions.erl
@@ -5,21 +5,27 @@
          get_value/2,
          set_value/3]).
 
+-define(STORAGE, builderl_ets_session_store).
+-define(SESSION_KEY, <<"session_id">>).
+
 request_start(Req) ->
     Cookies = cowboy_req:parse_cookies(Req),
     lager:debug("Cookies: ~p", [Cookies]),
     Req1 = init_cookies(Cookies, Req),
     {Cookies, Req1}.
 
-set_value(_Key, _Value, _Req) ->
-    ok.
+set_value(Key, Value, Cookies) ->
+    SID = proplists:get_value(?SESSION_KEY, Cookies),
+    ?STORAGE:set_value(Key, Value, SID).
 
-get_value(_Key, _Req) ->
-    ok.
+get_value(Key, Cookies) ->
+    SID = proplists:get_value(?SESSION_KEY, Cookies),
+    ?STORAGE:get_value(Key, SID).
 
 init_cookies([], Req) ->
     ID = uuid:uuid4(),
     SID = uuid:to_string(simple, ID),
-    cowboy_req:set_resp_cookie(<<"session_id">>, SID, Req, #{http_only => true});
+    cowboy_req:set_resp_cookie(<<"session_id">>, SID, Req, #{http_only => true,
+                                                             path => "/"});
 init_cookies(_, Req) ->
     Req.
